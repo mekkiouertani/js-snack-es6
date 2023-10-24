96ed2e0d51a8e87520e971eafbbd72d9426551b4
Mekki Ouertani <140953094+mekkiouertani@users.noreply.github.com>
2023-10-24 16:34:22 +0200

    add array students

--- js/scripit.js
+++ js/scripit.js
@@ -1,5 +1,6 @@
-//SNACK #1
 "use strict"
+
+//SNACK #1
 
 const guests = [ 
     'Brad Pitt', 
@@ -24,5 +25,53 @@ const printList = guests.map((guestName,i) => {
     return guest
 });
 
-console.log(printList);
+console.log("SNACK #1",printList);
 
+
+
+//SNACK #2
+
+const students = [
+    {
+        id : 213,
+        name : 'Marco della Rovere',
+        grades : 78
+    },
+    {
+        id : 110,
+        name : 'Paola Cortellessa',
+        grades : 96
+    },
+    {
+        id : 250,
+        name : 'Andrea Mantegna',
+        grades : 48
+    },
+    {
+        id : 145,
+        name : 'Gaia Borromini',
+        grades : 74
+    },
+    {
+        id : 196,
+        name : 'Luigi Grimaldello',
+        grades : 68
+    },
+    {
+        id : 102,
+        name : 'Piero della Francesca',
+        grades : 50
+    },
+    {
+        id : 120,
+        name : 'Francesca da Polenta',
+        grades : 84
+    },
+];
+
+const studentsNames = students.map((el)=>  el.name.toUpperCase());
+    
+   
+
+
+console.log(studentsNames);
