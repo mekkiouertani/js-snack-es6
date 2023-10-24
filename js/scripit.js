5b4a64ae74d29f909696596c6690ad575fb19129
Mekki Ouertani <140953094+mekkiouertani@users.noreply.github.com>
2023-10-24 16:22:39 +0200

    add array guests

---
+++ js/scripit.js
@@ -0,0 +1,18 @@
+//SNACK #1
+"use strict"
+
+const guests = [ 
+    'Brad Pitt', 
+    'Johnny Depp',
+    'Lady Gaga', 
+    'Cristiano Ronaldo', 
+    'Georgina Rodriguez', 
+    'Chiara Ferragni', 
+    'Fedez', 
+    'George Clooney', 
+    'Amal Clooney', 
+    'Maneskin'
+];
+
+
+
