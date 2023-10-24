41714ecef29732cf2bfce9cee893ce907476eaa8
Mekki Ouertani <140953094+mekkiouertani@users.noreply.github.com>
2023-10-24 17:29:44 +0200

    add snack #3

--- js/scripit.js
+++ js/scripit.js
@@ -78,3 +78,23 @@ console.log("SNACK # 2(grades)",studentGrades);
 const studentGradesId = students.filter((el)=> el.grades > 70 && el.id > 120);
 console.log("SNACK # 2(ID & grades)",studentGradesId);
 
+
+//SNACK # 3
+    /* A partire da un array di stringhe, crea un secondo array formattando le
+    stringhe del primo array in minuscolo e con l’iniziale maiuscola.
+    Es: [‘pippo’, ‘PLUTO’, ‘Paperino’] => [‘Pippo’, ‘Pluto’, ‘Paperino’] */
+
+const snack3 = [
+    "pippo",
+    "PLUTO",
+    "Paperino",
+    "MINNIE",
+    "topolinO"
+]
+
+const newSnack3 = snack3.map((el)=>{
+    let lower = el.toLowerCase()
+    let upper = lower.charAt(0).toUpperCase() + lower.slice(1)
+    return upper
+})
+console.log("SNACK 3", newSnack3);
