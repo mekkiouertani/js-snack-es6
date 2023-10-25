fe703928debe2a45b73d344f37386b8a8684a628
Mekki Ouertani <140953094+mekkiouertani@users.noreply.github.com>
2023-10-25 18:15:17 +0200

    add array snack #4

--- js/snack-2.js
+++ js/snack-2.js
@@ -24,6 +24,17 @@ const bikes = [
     },
 ];
 
+let biciMinore = bikes[0];
+
+bikes.forEach((bike)=>{
+    if (bike.weight < biciMinore.weight){
+        biciMinore.name = bike.name;
+        biciMinore.weight = bike.weight;
+    }
+});
+
+console.log(`SNACK #3: la bici più si chiama ${biciMinore.name} ed ha un peso di ${biciMinore.weight}kg`);
+
 /* const lowerWeightBike = [];
 
 bikes.forEach((bike)=>{
@@ -35,22 +46,37 @@ const bikeWithLowerWeight =  Math.min(...lowerWeightBike)
 console.log(`SNACK #3: La bici che pesa di meno :${bikeWithLowerWeight}kg`);
  */
 
-let biciMinore = bikes[0];
-
-bikes.forEach((bike)=>{
-    if (bike.weight < biciMinore.weight){
-        biciMinore.name = bike.name;
-        biciMinore.weight = bike.weight;
-    }
-});
-
-console.log(`la bici più si chiama ${biciMinore.name} ed ha un peso di ${biciMinore.weight}kg`);
-
-
-
 
 //SNACK #4
     /* Creare un array di oggetti di squadre di calcio. Ogni squadra avrà diverse proprietà: nome, punti fatti, falli subiti.
     Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0.
     Generare numeri random al posto degli 0 nelle proprietà: Punti fatti e falli subiti.
     Infine usando la destrutturazione creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console. */
+
+function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) ) + min;
+}
+
+const team = [
+    {
+        name: "Roma",
+        punti: 0,
+        falli: 0
+    },
+    {
+        name: "Milan",
+        punti: 0,
+        falli: 0
+    },
+    {
+        name: "Juventus",
+        punti: 0,
+        falli: 0
+    },
+    {
+        name: "Palermo",
+        punti: 0,
+        falli: 0
+    },
+];
+
